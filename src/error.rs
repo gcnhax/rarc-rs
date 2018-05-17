78ec3ce0fd25f281967f1850fe5963a38d072522
Erin <sylphofelectricity@gmail.com>
2018-05-17 11:37:53 -0500

    impl std::error::Error for Error

--- src/error.rs
+++ src/error.rs
@@ -1,4 +1,6 @@
 use std::io;
+use std::fmt;
+use std::error::Error as StdError;
 use nom;
 
 #[derive(Debug)]
@@ -25,5 +27,28 @@ impl From<io::Error> for Error {
 impl From<nom::ErrorKind> for Error {
     fn from(err: nom::ErrorKind) -> Error {
         Error::Parse(err)
+    }
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        match self {
+            Error::Io(io_err) => write!(f, "IO error: {}", io_err),
+            Error::Parse(parse_err) => write!(f, "Parse error: {}", parse_err),
+            Error::NameEncodingError(err) => write!(f, "Error encoding filename: {}", err),
+            _ => f.write_str(self.description()),
+        }
+    }
+}
+
+impl StdError for Error {
+    fn description(&self) -> &str {
+        match *self {
+            Error::Io(ref io_err) => io_err.description(),
+            Error::Parse(ref parse_err) => parse_err.description(),
+            Error::NameEncodingError(_) => "Error decoding filename",
+            Error::NoNodes => "No nodes present in node table",
+            Error::NoRootNode => "First node found in node table is not ROOT",
+        }
     }
 }
